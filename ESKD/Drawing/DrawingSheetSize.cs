3730c8668e469f40e972877c438e152a4505962a
jk-engineer <jk-218@yandex.ru>
2021-01-05 20:55:46 +0300

    Добавлен DrawingSheetSize

---
+++ ESKD/Drawing/DrawingSheetSize.cs
@@ -0,0 +1,145 @@
+﻿namespace ESKD.Drawing
+{
+    /// <summary>
+    /// Формат листа чертежа согласно ГОСТ 2.301.
+    /// </summary>
+    public static class DrawingSheetSize
+    {
+        /// <summary>
+        /// Формат листа чертежа согласно ГОСТ 2.301.
+        /// </summary>
+        public enum DrawingSheetSizeEnum
+        {
+            /// <summary>
+            /// Формат А0.
+            /// </summary>
+            A0,
+
+            /// <summary>
+            /// Формат А1.
+            /// </summary>
+            A1,
+
+            /// <summary>
+            /// Формат А2.
+            /// </summary>
+            A2,
+
+            /// <summary>
+            /// Формат А3.
+            /// </summary>
+            A3,
+
+            /// <summary>
+            /// Формат А4.
+            /// </summary>
+            A4,
+
+            /// <summary>
+            /// Формат А5.
+            /// </summary>
+            A5,
+
+            /// <summary>
+            /// Формат А0x2.
+            /// </summary>
+            A0x2,
+
+            /// <summary>
+            /// Формат А0x3.
+            /// </summary>
+            A0x3,
+
+
+            /// <summary>
+            /// Формат А1x3.
+            /// </summary>
+            A1x3,
+
+            /// <summary>
+            /// Формат А1x4.
+            /// </summary>
+            A1x4,
+
+            /// <summary>
+            /// Формат А2x3.
+            /// </summary>
+            A2x3,
+
+            /// <summary>
+            /// Формат А2x4.
+            /// </summary>
+            A2x4,
+
+            /// <summary>
+            /// Формат А2x5.
+            /// </summary>
+            A2x5,
+
+            /// <summary>
+            /// Формат А3x3.
+            /// </summary>
+            A3x3,
+
+            /// <summary>
+            /// Формат А3x4.
+            /// </summary>
+            A3x4,
+
+            /// <summary>
+            /// Формат А3x5.
+            /// </summary>
+            A3x5,
+
+            /// <summary>
+            /// Формат А3x6.
+            /// </summary>
+            A3x6,
+
+            /// <summary>
+            /// Формат А3x7.
+            /// </summary>
+            A3x7,
+
+            /// <summary>
+            /// Формат А4x3.
+            /// </summary>
+            A4x3,
+
+            /// <summary>
+            /// Формат А4x4.
+            /// </summary>
+            A4x4,
+
+            /// <summary>
+            /// Формат А4x5.
+            /// </summary>
+            A4x5,
+
+            /// <summary>
+            /// Формат А4x6.
+            /// </summary>
+            A4x6,
+
+            /// <summary>
+            /// Формат А4x7.
+            /// </summary>
+            A4x7,
+
+            /// <summary>
+            /// Формат А4x8.
+            /// </summary>
+            A4x8,
+
+            /// <summary>
+            /// Формат А4x9.
+            /// </summary>
+            A4x9,
+
+            /// <summary>
+            /// Нестандартный формат.
+            /// </summary>
+            NonStandard
+        }
+    }
+}
